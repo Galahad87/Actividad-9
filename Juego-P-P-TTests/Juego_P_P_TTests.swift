b90b902b5f2e2b1bdca0e1435e1a94e4176b7d91
Alumno IDS <alumnoids@iMac-de-IT-Admin.local>
2018-03-09 21:28:49 -0600

    Initial Commit

---
+++ Juego-P-P-TTests/Juego_P_P_TTests.swift
@@ -0,0 +1,36 @@
+//
+//  Juego_P_P_TTests.swift
+//  Juego-P-P-TTests
+//
+//  Created by Alumno IDS on 09/03/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import XCTest
+@testable import Juego_P_P_T
+
+class Juego_P_P_TTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
